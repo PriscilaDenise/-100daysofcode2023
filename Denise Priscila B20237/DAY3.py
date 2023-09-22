b21e65bebb0fec5de28b0b29ff64e95a45ac1f63
PriscilaDenise <126942316+PriscilaDenise@users.noreply.github.com>
2023-09-22 16:11:04 +0300

    Update DAY3.py

--- Denise Priscila B20237/DAY3.py
+++ Denise Priscila B20237/DAY3.py
@@ -1,7 +1,10 @@
+#give values that are to be multiplied
 num1 = 12
 num2 = 165
 num3 = 8
 num4 = 589
 num5 = 69
+#define the function multiply
 multiply = num1*num2*num3*num4*num5
+#display  the output 
 print(multiply)
