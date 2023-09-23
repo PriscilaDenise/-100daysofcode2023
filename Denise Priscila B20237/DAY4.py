18389a5fda246eefca5e9e24c119aaf16f9ae14a
PriscilaDenise <126942316+PriscilaDenise@users.noreply.github.com>
2023-09-23 21:08:45 +0300

    DAY4

---
+++ Denise Priscila B20237/DAY4.py
@@ -0,0 +1,16 @@
+# Declare variables
+name = "Alice"
+grade1 = 85
+grade2 = 90
+grade3 = 88
+
+# Calculate the average grade
+average = (grade1 + grade2 + grade3) / 3
+
+# Print the results
+print(f"Student Name: {name}")
+print(f"Assignment 1 Grade: {grade1}")
+print(f"Assignment 2 Grade: {grade2}")
+print(f"Assignment 3 Grade: {grade3}")
+print(f"Average Grade: {average:.2f}")
+
