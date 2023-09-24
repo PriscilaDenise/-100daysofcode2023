4610862c4dca94435414d84346dc4536d0bd6784
PriscilaDenise <126942316+PriscilaDenise@users.noreply.github.com>
2023-09-24 17:30:16 +0300

    DAY5

---
+++ Denise Priscila B20237/DAY5.py
@@ -0,0 +1,20 @@
+my_int = 20, 48, -95
+print(type(my_int))
+print(my_int)
+my_float = 2.25, 87.3569, 78.023
+print(type(my_float))
+print(my_float)
+my_string = "Denise"
+print(type(my_string))
+print(my_string)
+my_list = ["math", "english", "biology", "physics"]
+print(type(my_list))
+print(my_list)
+my_tuple = (402,89)
+print(type(my_tuple))
+print(my_tuple)
+my_dict = {'name':'Denise', 'tribe':'Musoga', 'course':'BSCS'}
+print(type(my_dict))
+print(my_dict)
+
+
