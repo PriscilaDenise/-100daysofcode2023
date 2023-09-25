c5f2eaa4da5246939b3577aacecf7c1d68158308
PriscilaDenise <126942316+PriscilaDenise@users.noreply.github.com>
2023-09-25 17:27:54 +0300

    DAY6

---
+++ Denise Priscila B20237/DAY6.py
@@ -0,0 +1,6 @@
+name = str(input("Enter your name: "))
+age = int(input("Enter your age: "))
+floating_point = float(input("Enter floating-point number: "))
+
+message = "Hi, {}! You are {} years old. Your floating point number is {}." . format(name, age, floating_point)
+print (message)
